d9547d47daa7d53568f8867189819b93045e00c7
YuriMont <81181134+YuriMont@users.noreply.github.com>
2021-06-28 18:00:31 -0300

    Melhora no projeto

--- src/program/Program.java
+++ src/program/Program.java
@@ -2,6 +2,8 @@
 package program;
 
 import java.util.Date;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
         Department obj = new Department(1, "Livros");
         Seller seler = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
         System.out.println(seler);
+        
+        SellerDao sellerDao = DaoFactory.createSellerDao();
     }
     
 }
