ccd1de7af39b90dc990e1dae77f5ff53bf204f1d
YuriMont <81181134+YuriMont@users.noreply.github.com>
2021-07-03 19:21:05 -0300

    Criado método em SellerDaoJDBC: update

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -60,7 +60,32 @@ public class SellerDaoJDBC implements SellerDao{
 
     @Override
     public void update(Seller obj) {
-        
+        PreparedStatement st = null;
+        try{
+            st = conn.prepareStatement("UPDATE seller " + 
+            "SET nome = ?, email = ?, data_nascimento = ?, baseSalary = ?, departamentoId = ? " +
+            "WHERE id = ?", Statement.RETURN_GENERATED_KEYS);
+            
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBithDate().getTime()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
+            st.setInt(6, obj.getId());
+            
+            int linhas = st.executeUpdate();
+            if(linhas>0){
+                System.out.println("Linhas modificada com sucesso! ");
+            }
+            else{
+                throw new DbException("Erro inesperado nenhuma linha foi afetada");
+            }
+            
+        }catch(SQLException e){
+            throw new DbException("Erro: "+e.getMessage());
+        }finally{
+            DB.closeStatement(st);
+        }
     }
 
     @Override
--- src/program/Program.java
+++ src/program/Program.java
@@ -28,6 +28,11 @@ public class Program {
          Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
          sellerDao.insert(newSeller);
          System.out.println(" id = "+newSeller.getId());
+         System.out.println("==== TEST 5: seller update ==== ");
+         seller = sellerDao.findById(1);
+         seller.setName("Martha Waine");
+         sellerDao.update(seller);
+         
          
     }
     
